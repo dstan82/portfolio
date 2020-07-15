9507ed7ed087415d89135cdddd322614fc887bb0
Ustorm <standragos82@gmail.com>
2020-07-15 17:49:19 +0300

    added steps for 2nd challenge

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Create a blog models
+# title
+# pub_dat
+# body
+# image
+
+# Add the Blog app to the settings
+
+# Create a migration
+
+# Migrate
+
+# Add to the admin
