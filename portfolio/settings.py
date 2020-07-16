6b9f3c329463013e81da4b1613d4a56ae1784224
Ustorm <standragos82@gmail.com>
2020-07-16 19:56:48 +0300

    migrated to gcp

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': '12345',
+        'PASSWORD': '1234',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
