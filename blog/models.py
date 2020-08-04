410ed11a32f5f38faa76daddbf505118c9d0db37
Lenovo <standragos82@gmail.com>
2020-08-04 12:24:10 +0300

    added breaklines in blog body

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+from .fields import *
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
-    body = models.TextField()
+    body = NonStrippingTextField()
     image = models.ImageField(upload_to='images/')
 
     def summary(self):
--- jobs/models.py
+++ jobs/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    title = models.CharField(max_length=50, default='Please add a title')
+
+    def __str__(self):
+        return self.title
