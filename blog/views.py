cdea7c46d18ddb875555734e104da45ba56c5183
Lenovo <standragos82@gmail.com>
2020-08-04 17:35:11 +0300

    fixed blog order

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 # Create your views here.
 def allblogs(request):
-    posts = Post.objects
+    posts = Post.objects.order_by('-pub_date')
     return render(request, 'blog/allblogs.html', {'posts':posts})
 
 def detail(request, blog_id):
