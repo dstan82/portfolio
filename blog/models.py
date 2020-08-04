80d802ad150e966f2f424ce6da120cc63965f95a
Ustorm <standragos82@gmail.com>
2020-08-04 16:20:00 +0300

    changed top menu

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from .fields import *
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
-    body = NonStrippingTextField()
+    body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
     def summary(self):
